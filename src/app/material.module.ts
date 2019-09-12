c181c2ba8147c3a07fc5e21664560ef316d246db
Emily Schlicter <35039378+eschlicter@users.noreply.github.com>
2019-09-12 10:47:54 -0500

    split nav components, functional

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -7,6 +7,9 @@ import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
     imports: [
@@ -16,7 +19,10 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
         MatInputModule,
         MatDatepickerModule,
         MatNativeDateModule,
-        MatCheckboxModule
+        MatCheckboxModule,
+        MatSidenavModule,
+        MatToolbarModule,
+        MatListModule
     ],
     exports: [
         MatButtonModule,
@@ -25,7 +31,10 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
         MatInputModule,
         MatDatepickerModule,
         MatNativeDateModule,
-        MatCheckboxModule
+        MatCheckboxModule,
+        MatSidenavModule,
+        MatToolbarModule,
+        MatListModule
     ]
 })
 
